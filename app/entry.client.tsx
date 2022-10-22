93e569bf2d8b46897c1430ebc52249624efa051f
Anand Chowdhary <mail@anandchowdhary.com>
2022-10-22 18:52:54 +0200

    chore: Init repo with Mantine & Remix

---
+++ app/entry.client.tsx
@@ -0,0 +1,25 @@
+import { RemixBrowser } from "@remix-run/react";
+import { ClientProvider } from "@mantine/remix";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <ClientProvider>
+          <RemixBrowser />
+        </ClientProvider>
+      </StrictMode>
+    );
+  });
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1);
+}
